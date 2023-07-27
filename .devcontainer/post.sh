2b76a359411707e244c2f30df87ec68cba02a7e1
Jordan Golightly <mail@jordangolightly.com>
2023-07-27 17:03:20 +0000

    post script update

--- .devcontainer/post.sh
+++ .devcontainer/post.sh
@@ -1,1 +1,14 @@
 echo -e "\nexport PATH=$PATH:/usr/local/go/bin:/home/jordan/node/bin:/home/jordan/w/statyk" >> /home/jordan/.bashrc
+
+source /home/jordan/.bashrc
+
+go version
+node --version
+npm --version
+
+
+go install -v github.com/ramya-rao-a/go-outline@v0.0.0-20210608161538-9736a4bde949
+go install -v golang.org/x/tools/gopls@latest
+
+npm i -g npm@latest
+npm i -g sass
