338e21c0f366c45d4e015500fb4cd9b8d1a79b9f
Jordan Golightly <mail@jordangolightly.com>
2023-07-27 16:56:48 +0000

    New post attach command in devcontainer

--- .devcontainer/post.sh
+++ .devcontainer/post.sh
@@ -1,1 +1,1 @@
-export PATH=$PATH:/usr/local/go/bin:/home/jordan/node/bin:/home/jordan/w/statyk
+echo -e "\nexport PATH=$PATH:/usr/local/go/bin:/home/jordan/node/bin:/home/jordan/w/statyk" >> /home/jordan/.bashrc
